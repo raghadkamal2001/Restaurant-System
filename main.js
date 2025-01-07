c4b9fadf04c56ad48d1c5469d48b13cbf85862a9
raghadkamal2001 <raghadkamall2001@gmail.com>
2025-01-07 08:44:18 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+document.getElementById('Fullname').value;
+document.getElementById('password').value;
+document.getElementById('Birth').value;
+document.getElementById('gender').value;
+document.getElementById('phone').value;
+document.getElementById('combo').value;
+document.getElementById('sandwish').value;
+document.getElementById('botton').value;
+
+
+
+function render(){
+
+    let fullname= BirthElement.value;
+}
